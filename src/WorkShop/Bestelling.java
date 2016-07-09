2a1b26e8abb07f73c1b2cdbbc23159bd4b5d40b6
Excen <Jens_simons@outlook.com>
2016-07-09 13:03:12 +0200

    Update DAO's Bestelling

--- src/WorkShop/Bestelling.java
+++ src/WorkShop/Bestelling.java
@@ -16,19 +16,99 @@ public class Bestelling {
     private int bestelling_id;
     private int klant_id;
     private int artikel_id1;
-    private int artikel_id2;
-    private int artikel_id3;
+
     private String artikel_naam1;
-    private String artikel_naam2;
-    private String artikel_naam3;
+
     private int artikel_aantal1;
-    private int artikel_aantal2;
-    private int artikel_aantal3;
+
     private float artikel_prijs1;
-    private float artikel_prijs2;
-    private float artikel_prijs3;
+
     
     public Bestelling(){};
+
+    /**
+     * @return the bestelling_id
+     */
+    public int getBestelling_id() {
+        return bestelling_id;
+    }
+
+    /**
+     * @param bestelling_id the bestelling_id to set
+     */
+    public void setBestelling_id(int bestelling_id) {
+        this.bestelling_id = bestelling_id;
+    }
+
+    /**
+     * @return the klant_id
+     */
+    public int getKlant_id() {
+        return klant_id;
+    }
+
+    /**
+     * @param klant_id the klant_id to set
+     */
+    public void setKlant_id(int klant_id) {
+        this.klant_id = klant_id;
+    }
+
+    /**
+     * @return the artikel_id1
+     */
+    public int getArtikel_id1() {
+        return artikel_id1;
+    }
+
+    /**
+     * @param artikel_id1 the artikel_id1 to set
+     */
+    public void setArtikel_id1(int artikel_id1) {
+        this.artikel_id1 = artikel_id1;
+    }
+
+    /**
+     * @return the artikel_naam1
+     */
+    public String getArtikel_naam1() {
+        return artikel_naam1;
+    }
+
+    /**
+     * @param artikel_naam1 the artikel_naam1 to set
+     */
+    public void setArtikel_naam1(String artikel_naam1) {
+        this.artikel_naam1 = artikel_naam1;
+    }
+
+    /**
+     * @return the artikel_aantal1
+     */
+    public int getArtikel_aantal1() {
+        return artikel_aantal1;
+    }
+
+    /**
+     * @param artikel_aantal1 the artikel_aantal1 to set
+     */
+    public void setArtikel_aantal1(int artikel_aantal1) {
+        this.artikel_aantal1 = artikel_aantal1;
+    }
+
+    /**
+     * @return the artikel_prijs1
+     */
+    public float getArtikel_prijs1() {
+        return artikel_prijs1;
+    }
+
+    /**
+     * @param artikel_prijs1 the artikel_prijs1 to set
+     */
+    public void setArtikel_prijs1(float artikel_prijs1) {
+        this.artikel_prijs1 = artikel_prijs1;
+    }
     
     
     
