54c39eae2bb3e4ca4b87318410b04e707392db0d
= <anne.hoogerbrugge@gmail.com>
2016-07-18 23:09:31 +0200

    update opvangmethoden

--- src/DAOs/Impl/ArtikelDAOImpl.java
+++ src/DAOs/Impl/ArtikelDAOImpl.java
@@ -1,4 +1,3 @@
-
 package DAOs.Impl;
 
 import DAOs.Interface.ArtikelDAOInterface;
@@ -8,187 +7,190 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
 public class ArtikelDAOImpl implements ArtikelDAOInterface {
-    
+
     Connection con;
     ResultSet rs;
-    PreparedStatement stmt;
+    PreparedStatement pstmt;
     Statement st;
     String driver = "com.mysql.jdbc.Driver";
     String url = "jdbc:mysql://localhost:3306/winkel?autoReconnect=true&useSSL=false";
     String gebruikersNaam = "Anjewe";
     String wachtwoord = "Koetjes";
-   
-   
+
     @Override
     public ArrayList<Artikel> findAll() {
         ArrayList<Artikel> artikelList = new ArrayList<>();
         try {
             Class.forName(driver);
-            con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord); 
+            con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
             String sqlQuery = "select * from artikel";
-            stmt = con.prepareStatement(sqlQuery);
-            rs = stmt.executeQuery();
-        
-        while (rs.next()) {
-            // get the fields from one artikel and store it in an Artikel object
-            Artikel artikel = new Artikel();
-            artikel.setArtikelID(rs.getInt("artikel_id"));
-            artikel.setArtikelNaam(rs.getString("artikel_naam"));
-            artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
-                     
-            // add the artikel in the list
-            artikelList.add(artikel);
+            pstmt = con.prepareStatement(sqlQuery);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                // get the fields from one artikel and store it in an Artikel object
+                Artikel artikel = new Artikel();
+                artikel.setArtikelID(rs.getInt("artikel_id"));
+                artikel.setArtikelNaam(rs.getString("artikel_naam"));
+                artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
+
+                // add the artikel in the list
+                artikelList.add(artikel);
             }
-        }
-        catch (SQLException | ClassNotFoundException ex) {
+        } catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Data search failed");
             ex.printStackTrace();
-            }  
-        
-        return artikelList;  
         }
-           
 
-            
+        return artikelList;
+    }
+
     @Override
-    public Artikel findByArtikelID (int artikelID) {
+    public Artikel findByArtikelID(int artikelID) {
         Artikel artikel = new Artikel();
         try {
             Class.forName(driver);
             con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
-            String sqlQuery = "select * from artikel where artikel_id = " + artikelID;
-            stmt = con.prepareStatement(sqlQuery);
-            rs = stmt.executeQuery();
-        
-            while (rs.next()) {
+            String sqlQuery = "select * from artikel where artikel_id = ? ";
+            pstmt = con.prepareStatement(sqlQuery);
+            pstmt.setInt(1, artikelID);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
                 artikel.setArtikelID(rs.getInt("artikel_id"));
                 artikel.setArtikelNaam(rs.getString("artikel_naam"));
                 artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
             }
-        }
+            else {
+                throw new NullPointerException(); // is nog nodig, of kan gewoon printstatement in else?
+            }
+        } 
+        catch (NullPointerException ex) {
+            System.out.println("Dit artikel naam bestaat niet, probeer opnieuw. ");
+        } 
         catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Data search failed");
             ex.printStackTrace();
-            }  
-        return artikel;
         }
-    
+        return artikel;
+    }
+
     @Override
-    public Artikel findByArtikelNaam (String artikelNaam) {
+    public Artikel findByArtikelNaam(String artikelNaam) {
         Artikel artikel = new Artikel();
         try {
             Class.forName(driver);
             con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
-            boolean continueInput = true;
-            
-            do {
-                try {
-                    String SQLZoeken = "select count(artikel_naam) from artikel where artikel_naam = " + artikelNaam;
-                    ResultSet zoeken = stmt.executeQuery(SQLZoeken);
-                    if (zoeken.next()) {
-                        try {
-                           String sqlQuery = "select * from artikel where artikel_naam = ? "; 
-                           stmt = con.prepareStatement(sqlQuery);
-                           stmt.setString(1, artikelNaam);
-                           rs = stmt.executeQuery();
-                           while (rs.next()) {
-                                artikel.setArtikelID(rs.getInt("artikel_id"));
-                                artikel.setArtikelNaam(rs.getString("artikel_naam"));
-                                artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
-                            }
-                            continueInput = false;    
-                        }
-                        finally {
-                            con.close();
-                        }
-                    }
-                }
-            catch(NullPointerException ex){
-                System.out.println("Dit artikel naam bestaat niet, probeer opnieuw: ");
-                Scanner input = new java.util.Scanner(System.in);
-                artikelNaam = input.nextLine();
-                }
-            }while (continueInput);
-       }  
+            String SQLZoeken = "select * from artikel where artikel_naam = '" + artikelNaam + "'";
+            pstmt = con.prepareStatement(SQLZoeken);
+            rs = pstmt.executeQuery(SQLZoeken);
+            if (rs.next()) {
+                artikel.setArtikelID(rs.getInt("artikel_id"));
+                artikel.setArtikelNaam(rs.getString("artikel_naam"));
+                artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
+            } 
+            else {
+                throw new NullPointerException(); // is nog nodig, of kan gewoon printstatement in else?
+            }
+        } 
+        catch (NullPointerException ex) {
+            System.out.println("Dit artikel naam bestaat niet, probeer opnieuw. ");
+        } 
         catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Data search failed");
             ex.printStackTrace();
         }
-    return artikel;   
-    }         
-                
-        
+        return artikel;
+    }
 
-    
+   
     @Override
-    public Artikel findByArtikelPrijs (double artikelPrijs) {
+    public Artikel findByArtikelPrijs(double artikelPrijs) {
         Artikel artikel = new Artikel();
         try {
             Class.forName(driver);
             con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
             String sqlQuery = "select * from artikel where artikel_prijs = " + artikelPrijs;
-            stmt = con.prepareStatement(sqlQuery);
-            rs = stmt.executeQuery();
-        
-            while (rs.next()) {
+            pstmt = con.prepareStatement(sqlQuery);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
                 artikel.setArtikelID(rs.getInt("artikel_id"));
                 artikel.setArtikelNaam(rs.getString("artikel_naam"));
                 artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
             }
-        }
+            else {
+                throw new NullPointerException(); // is nog nodig, of kan gewoon printstatement in else?
+            }
+        } 
+        catch (NullPointerException ex) {
+            System.out.println("Dit artikel naam bestaat niet, probeer opnieuw.");    
+        } 
         catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Data search failed");
             ex.printStackTrace();
-            }        
-        return artikel;
-       
         }
-   @Override
-   public boolean insertArtikel (int artikelID, String artikelNaam, double artikelPrijs) {
-       //boolean return als gelukt is? 
-            boolean isAdded = true;
-        try {    
+        return artikel;
+
+    }
+
+    @Override
+    public boolean insertArtikel(int artikelID, String artikelNaam, double artikelPrijs) {
+        //boolean return als gelukt is? 
+        boolean isAdded = true;
+        try {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
             String sqlUpdate = "insert into artikel (artikel_id, artikel_naam, artikel_prijs)"
-                + "values (?, ?, ?)";
-            stmt = con.prepareStatement(sqlUpdate);
-        
-            stmt.setInt(1, artikelID);
-            stmt.setString(2, artikelNaam);
-            stmt.setDouble(3, artikelPrijs);
-        
-            stmt.executeUpdate();
-        }
+                    + "values (?, ?, ?)";
+            pstmt = con.prepareStatement(sqlUpdate);
+
+            pstmt.setInt(1, artikelID);
+            pstmt.setString(2, artikelNaam);
+            pstmt.setDouble(3, artikelPrijs);
+
+            pstmt.executeUpdate();
+        } 
         catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Data entry failed.");
             ex.printStackTrace();
             isAdded = false;
         }
-            return isAdded;
+        return isAdded;
     }
 
-    
     // delete methode
-   @Override
-    public void deleteArtikel(int artikelID) {
-        try { 
-            Class.forName(driver);
-            con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
+    @Override
+    public boolean deleteArtikel(int artikelID) {
+        boolean isDeleted = false;
+        try {
+           Class.forName(driver);
+           con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
+           String artikelZoeken = "select * from artikel where artikel_id = " + artikelID;
+           pstmt = con.prepareStatement(artikelZoeken);
+           rs = pstmt.executeQuery(artikelZoeken);
+           boolean artikelFound = rs.next();
+           
+           if (artikelFound) {
             String sqlUpdate = "delete from artikel where artikel_id = ?";
-            stmt = con.prepareStatement(sqlUpdate);
-                    
-            stmt.setInt(1, artikelID);
-            stmt.executeUpdate();
+            pstmt = con.prepareStatement(sqlUpdate);
+            pstmt.setInt(1, artikelID);
+            pstmt.executeUpdate(); 
+            isDeleted = true;
             }
+           
+           else {
+               System.out.println("Het artikel met artikel id " + artikelID + " staat niet in de database.");
+           }
+        }
         catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Deleting failed.");
             ex.printStackTrace();
         }
+        return isDeleted;
     }
+    
     
     // update method
     @Override
@@ -197,19 +199,19 @@ public class ArtikelDAOImpl implements ArtikelDAOInterface {
             Class.forName(driver);
             con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
             String sqlUpdate = "update artikel set artikel_id = ?, artikel_naam = ?, artikel_prijs = ?";
-            stmt = con.prepareStatement(sqlUpdate);
-        
-            stmt.setInt(1, artikel.getArtikelID());
-            stmt.setString(2, artikel.getArtikelNaam());
-            stmt.setDouble(3, artikel.getArtikelPrijs());
-        
-            stmt.executeUpdate();
-        }
+            pstmt = con.prepareStatement(sqlUpdate);
+
+            pstmt.setInt(1, artikel.getArtikelID());
+            pstmt.setString(2, artikel.getArtikelNaam());
+            pstmt.setDouble(3, artikel.getArtikelPrijs());
+
+            pstmt.executeUpdate();
+        } 
         catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Update failed.");
             ex.printStackTrace();
         }
-      
+
     }
-    
+
 }
---
+++ src/MAIN/ArtikelView.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MAIN;
+page 
+
+import DAOs.Impl.ArtikelDAOImpl;
+
+/**
+ *
+ * @author Anne
+ */
+public class ArtikelView {
+    public String zoekOpArtikelNaam(){
+        Scanner scan = new Scanner();
+        String artikelNaam = scan.next();
+        return artikelNaam;
+
+    }
+    
+    publc void printArtikel(Artikel artikellen){
+        
+    }
+}
+
+
+public class ArtikelController{
+    public void vindbestlelingenmetaritkel(){
+        ArtikelView view = new ArtikelView();
+        String ArtikelNaam = view.zoekOpArtikelNaam();
+        
+        
+        KoppeltabalDAO Kdao = new kopelakfjalfj()
+        ArrayList<besetling> =        kdao findByArtikelID(artikel.getID());
+        
+        ArtikelDAOImpl dao = new ArtikelDAOImpl();
+        Artikel artikel =  dao.findByArtikelNaam(ArtikelNaam);
+        
+        
+        view.printBestellingMetArtikel()
+        
+    }
+    
+    
+}
